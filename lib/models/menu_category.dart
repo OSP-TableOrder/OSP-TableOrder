6f57ff29876fbff401c744c7a30eb4403282bcf7
hhhyenn <hyenn@kumoh.ac.kr>
2025-11-22 01:51:06 +0900

    refactor: MenuCategory 모델에 id 필드 추가

--- lib/models/menu_category.dart
+++ lib/models/menu_category.dart
@@ -1,10 +1,12 @@
 import 'package:table_order/models/menu.dart';
 
 class MenuCategory {
+  final int id;
   final String name;
   final List<Menu> items;
 
   MenuCategory({
+    required this.id,
     required this.name, 
     required this.items
   });
