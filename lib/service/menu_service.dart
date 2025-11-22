148307050f6e86947dfec62a781ec235e56f1239
hhhyenn <hyenn@kumoh.ac.kr>
2025-11-23 03:45:20 +0900

    fix: Server 인스턴스를 static으로 변경하여 메모리 사용 최적화

--- lib/service/menu_service.dart
+++ lib/service/menu_service.dart
@@ -3,7 +3,7 @@ import 'package:table_order/server/user_server.dart';
 
 class MenuService {
   // Mock API
-  final Server _server = Server();
+  static final Server _server = Server();
 
   // 전체 메뉴 목록 가져오기
   Future<List<Menu>> fetchAllMenus() => _server.getAllMenus();
--- lib/service/store_service.dart
+++ lib/service/store_service.dart
@@ -3,7 +3,7 @@ import 'package:table_order/server/user_server.dart';
 
 class StoreService {
   // Mock API
-  final Server _server = Server();
+  static final Server _server = Server();
 
   // 전체 가게 목록 가져오기
   Future<List<Store>> fetchAllStores() => _server.getAllStores();
