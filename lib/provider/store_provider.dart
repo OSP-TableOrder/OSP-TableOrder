0294ed34b5e8dba2790afaed11cb70a2fb0161b9
hhhyenn <hyenn@kumoh.ac.kr>
2025-11-23 03:25:38 +0900

    feat: StoreProvider에서 가게 데이터 가져오는 메서드 이름을 fetch로 변경

--- lib/provider/store_provider.dart
+++ lib/provider/store_provider.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/foundation.dart';
 
-import 'package:table_order/service/store_service.dart';
 import 'package:table_order/models/store.dart';
+import 'package:table_order/service/store_service.dart';
 
 class StoreProvider with ChangeNotifier {
   final StoreService _storeService = StoreService();
@@ -9,17 +9,19 @@ class StoreProvider with ChangeNotifier {
 
   List<Store> get stores => _stores;
 
+  // TODO : 아래 로직들 비동기 처리 필요
+
   StoreProvider() {
     // 생성자에서 데이터 초기화
-    _stores = _storeService.getAllStores();
-  }
+    _stores = _storeService.fetchAllStores();
+  } 
 
   // 특정 ID로 가게 찾기
-  Store? findById(int id) => _storeService.getStoreById(id);
+  Store? findById(int id) => _storeService.fetchStoreById(id);
 
   // 나중에 서버 데이터 새로고침용
   void refreshStores() {
-    _stores = _storeService.getAllStores();
+    _stores = _storeService.fetchAllStores();
     notifyListeners();
   }
 }
